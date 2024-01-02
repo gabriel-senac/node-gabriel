0736bd10bd63a0ebdd881d5914d5a7d286608ebf
Prof Cleber <cdfelipe@hotmail.com>
2024-01-02 08:56:04 -0300

    atualização controller;js completo

--- src/modules/teste/infra/controller.js
+++ src/modules/teste/infra/controller.js
@@ -14,41 +14,35 @@ export class Controller{
     }
 
     async postTeste(request, response){
-        const {variavel_teste} = request.body;
-        if (variavel_teste == null || variavel_teste === '') {
-            return response.status(400).json({message: "FALTOU DADO AE MACHO"});
-        }
+        const {produto, preco, quantidade, dataVenda} = request.body;
 
         try {
-            return response.status(200).json({message: "Dado enviado: " + variavel_teste})
+            const [rows] = await connection.query('INSERT INTO venda(produto, preco, quantidade, datavenda) VALUES (?,?,?,?)', [produto, preco, quantidade, dataVenda]);
+            return response.status(200).json({message: "Produto criado com sucesso! "})
         } catch (error) {
-            return response.status(500).json({message: "OPS... dado não enviado! "})
+            return response.status(500).json({message: "OPS... produto não foi criado! "})
         }
     }
 
     async putTeste(request, response){
-        const {variavel_teste} = request.body;
-        if (variavel_teste == null || variavel_teste === '') {
-            return response.status(404).json({message: "FALTOU DADO AE MACHO"});
-        }
+        const {idvenda, produto, preco, quantidade, dataVenda} = request.body;
 
         try {
-            return response.status(200).json({message: "Dado enviado: " + variavel_teste})
+            const [rows] = await connection.query('UPDATE venda SET produto=?, preco=?, quantidade=?, datavenda=? WHERE idvenda=?', [produto, preco, quantidade, dataVenda, idvenda]);
+            return response.status(200).json({message: "Venda atualizada com sucesso!!! " })
         } catch (error) {
-            return response.status(500).json({message: "OPS... dado não enviado! "})
+            return response.status(500).json({message: "OPS... venda não atualizada! "})
         }
     }
 
     async deleteTeste(request, response){
-        const {variavel_teste} = request.body;
-        if (variavel_teste == null || variavel_teste === '') {
-            return response.status(400).json({message: "FALTOU DADO AE MACHO"});
-        }
+        const {idvenda} = request.body;
 
         try {
-            return response.status(200).json({message: "Dado enviado: " + variavel_teste})
+            const [rows] = await connection.query('DELETE FROM venda WHERE idvenda=?', [idvenda]);
+            return response.status(200).json({message: "Venda excluída com sucesso!"})
         } catch (error) {
-            return response.status(500).json({message: "OPS... dado não enviado! "})
+            return response.status(500).json({message: "OPS... venda não excluída! "})
         }
     }
 
